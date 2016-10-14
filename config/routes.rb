aed0e66beebb5665a1aa581b00718b08d8f85cc5
Matt <mpennaz@gmail.com>
2016-10-14 13:01:31 +0000

    Added functionalty

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,8 @@ Rails.application.routes.draw do
   
   root 'users#my_portfolio'
   
+  #root 'welcome#index'
+  
   get 'my_portfolio', to: 'users#my_portfolio'
   
   get 'search_stocks', to: 'stocks#search'
